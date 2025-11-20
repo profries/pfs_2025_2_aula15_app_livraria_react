92212daed81496612fb2067c4ee095799c3cce49
Luis Henrique Ries <prof.ries@gmail.com>
2025-11-19 21:23:05 -0300

    Acesso à API (GET /livros) utilizando Axios e useEffect.

--- src/components/ListCardLivros.tsx
+++ src/components/ListCardLivros.tsx
@@ -1,11 +1,25 @@
-import listaLivros from "../data/Livros.json"
-import CardLivro from "./CardLivro"
+import { useEffect, useState } from "react";
+import CardLivro from "./CardLivro";
+import LivroApiService from "../service/LivroApiService";
 
+interface Livro {
+    id: string;
+    imagem: string;
+    titulo: string;
+    preco: number;
+}
 
 export default function ListCardLivros() {
-    console.log(listaLivros);
+    const [listaLivros, setListaLivros] = useState([]);
+
+    useEffect(() => {
+        LivroApiService.listar().then(
+            livros => setListaLivros(livros)
+        )
+    }, [])
+
     return (
-        listaLivros.map(livro => 
+        listaLivros.map((livro:Livro) => 
             <CardLivro key={livro.id} livro={livro} />
         )    
     )
---
+++ src/service/LivroApiService.ts
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const URI = "http://localhost:3000/livros";
+
+ async function listar() {
+    const response = await axios.get(URI);
+    return response.data;
+}
+
+export default {
+    listar
+}
+
